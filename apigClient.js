c09f447bbd2cbd82e5ac41a2de8d665e603315f6
Onel-R <leonreuben@yahoo.com>
2025-10-01 12:56:46 -0400

    Update apigClient.js

--- apigClient.js
+++ apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://q45ivfqalj.execute-api.us-east-1.amazonaws.com/prod';
+    var invokeUrl = 'https://7p0ljoqnqa.execute-api.us-east-1.amazonaws.com/prod';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
